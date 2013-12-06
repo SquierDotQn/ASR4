6e45f38b3d03d15cb487ad60136e6e8da137049f
SquierDotQn <gamefreak35@hotmail.fr>
2013-12-06 11:41:40 +0100

    Create malloc.h

---
+++ malloc.h
@@ -0,0 +1,18 @@
+#define HEAP_SIZE 50
+#define FREE 1
+#define BUSY 0
+
+typedef struct{
+unsigned int free ;
+unsigned int size ;
+/* ces champs ne sont pas écrit dans la heap
+mais sont affectés par get_chunk */
+unsigned char * addr ;
+unsigned char * next_chunk ;
+unsigned char * previous_chunk ;
+}chunk;
+
+unsigned char heap[HEAP_SIZE];
+
+unsigned int get_int(void *ptr);
+void set_int(void *ptr, unsigned int val);
