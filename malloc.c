69855caead22ce4d77d734ab17748236538881c3
SquierDotQn <gamefreak35@hotmail.fr>
2013-12-06 11:41:22 +0100

    Create malloc.c

---
+++ malloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "malloc.h"
+
+
+/* Q4 bis &(s[i])
+void display_raw();
+Q4 ter
+void display_chucks();*/
+
+
+unsigned int get_int(void *ptr){
+	unsigned int i = (int)*ptr;
+	return i;
+}
+void set_int(void *ptr, unsigned int val){
+
+}
