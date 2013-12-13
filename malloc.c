506146d53a0f1626154a51d0ec605ecbf5d8c218
Théo Plockyn <gamefreak35@hotmail.fr>
2013-12-13 10:58:39 +0100

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -9,9 +9,39 @@ void display_chucks();*/
 
 
 unsigned int get_int(void *ptr){
-	unsigned int i = (int)*ptr;
-	return i;
+        unsigned int i = *(unsigned int*)ptr;
+        return i;
 }
 void set_int(void *ptr, unsigned int val){
-	(*int)ptr = &val;
+        unsigned int* iptr = (unsigned int*) ptr;
+        *iptr = val;
 }
+
+
+void set_chunk(chunk *c, unsigned char *ptr){
+	set_int(ptr,c->free);
+	set_int(ptr + sizeof(unsigned int),c->size);
+	set_int( (ptr + c->size - sizeof(unsigned int)) ,c->size);
+}
+
+void get_chunk(chunk *c, unsigned char *ptr){
+	c->free = get_int(ptr);
+	c->size = get_int(ptr + sizeof(unsigned int));
+	c->addr = ptr;
+	if(ptr == heap)
+		c->previous_chunk = NULL;
+	else
+		c->previous_chunk = ptr - get_int(ptr-sizeof(unsigned int));
+	if(ptr + get_int(ptr+sizeof(unsigned int))==&heap[get_int(ptr+sizeof(unsigned int))])
+		c->next_chunk = ptr + get_int(ptr+sizeof(unsigned int));
+	else
+		c->next_chunk = NULL;
+}
+
+void init_alloc(){
+	chunk init;
+	init.free = FREE;
+	init.size = HEAP_SIZE;
+	set_chunk(&init, heap);
+}
+
