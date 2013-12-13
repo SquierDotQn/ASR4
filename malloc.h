b117f315062e45386e4403ea46e269ab194697a0
Théo Plockyn <gamefreak35@hotmail.fr>
2013-12-13 10:58:15 +0100

    Update malloc.h

--- malloc.h
+++ malloc.h
@@ -3,16 +3,19 @@
 #define BUSY 0
 
 typedef struct{
-unsigned int free ;
-unsigned int size ;
-/* ces champs ne sont pas écrit dans la heap
-mais sont affectés par get_chunk */
-unsigned char * addr ;
-unsigned char * next_chunk ;
-unsigned char * previous_chunk ;
+	unsigned int free ;
+	unsigned int size ;
+	/* ces champs ne sont pas écrit dans la heap
+	mais sont affectés par get_chunk */
+	unsigned char * addr ;
+	unsigned char * next_chunk ;
+	unsigned char * previous_chunk ;
 }chunk;
 
 unsigned char heap[HEAP_SIZE];
 
 unsigned int get_int(void *ptr);
 void set_int(void *ptr, unsigned int val);
+void set_chunk(chunk *c, unsigned char *ptr);
+void get_chunk(chunk *c, unsigned char *ptr);
+void init_alloc();
